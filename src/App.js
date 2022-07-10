211b323be8207324d2ab85c78475f0900dff484e
ZuciEC <zucidemartinez@gmail.com>
2022-07-10 08:28:51 -0700

    Cards

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Bay from './components/dashboard/Bay';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl dark:bg-slate-800 bg-blue-200 font-bold underline">
-        Putos
-      </h1>
       <Bay></Bay>
     </div>
   );
--- src/components/dashboard/Bay.js
+++ src/components/dashboard/Bay.js
@@ -2,14 +2,98 @@ import './Bay.css';
 import React, {useState,useEffect} from "react";
 import { Fragment } from "react";
 
+
 const Bay = () => {
-    <Fragment>
-        return(
-            <div className="Bay">
-                <h1 className="title"> Zuci </h1>
+    return(
+        <Fragment>
+            <div className="content">
+                <div className="Bay">
+                    <h1 className="title"> Bahia 1 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i className="uil uil-arrow-circle-up"></i>
+                            <label className="time"> 10:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">3</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
+                <div className="Bay">
+                    <h1 className="title"> Bahia 2 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i className="uil uil-arrow-circle-down"> </i>
+                            <label className="time"> 10:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">3</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
+                <div className="Bay">
+                    <h1 className="title"> Bahia 3 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i class="uil uil-clock"> </i>
+                            <label className="time"> 00:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">0</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
+                <div className="Bay">
+                    <h1 className="title"> Bahia 4 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i className="uil uil-minus-circle"> </i>
+                            <label className="time"> 00:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">0</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
+                <div className="Bay">
+                    <h1 className="title"> Bahia 5 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i className="uil uil-arrow-circle-down"> </i>
+                            <label className="time"> 07:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">5</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
+                <div className="Bay">
+                    <h1 className="title"> Bahia 6 </h1>
+                    <div className="time-person">
+                        <div className="time-content">
+                            <i className="uil uil-arrow-circle-up"> </i>
+                            <label className="time">12:00 </label>
+                        </div>
+                        <div className="person-content">
+                            <i className="uil uil-user-md"> </i>
+                            <label className="person">2</label>
+                        </div>
+                    </div>
+                    <div className="chart"></div>
+                </div>
             </div>
-        );
-    </Fragment>
+        </Fragment>
+    );
 
 }
 export default Bay;
