47e38210a0ee82f171023ac5f895b68b869451e6
ZuciEC <zucidemartinez@gmail.com>
2022-07-09 22:56:15 -0700

    A ver

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-3xl dark:bg-slate-800 bg-blue-200 font-bold underline">
-        Hello world!
+        Putos
       </h1>
     </div>
   );
