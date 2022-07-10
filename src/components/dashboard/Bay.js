2c1e7dd84aceafd06ea45196c466852cfdc4d941
ZuciEC <zucidemartinez@gmail.com>
2022-07-09 23:47:43 -0700

    a

--- src/components/dashboard/Bay.js
+++ src/components/dashboard/Bay.js
@@ -1,11 +1,12 @@
 import './Bay.css';
+import React, {useState,useEffect} from "react";
+import { Fragment } from "react";
 
-function Bay() {
+const Bay = () => {
     return(
         <div className="Bay">
             <h1 className="title"> Zuci </h1>
         </div>
     );
 }
-
 export default Bay;
