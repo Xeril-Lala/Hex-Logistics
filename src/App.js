30cfc9f281d7d291bb051cd7d559c94c721ccc3f
Xeril-Lala <lopezlopezabel96@gmail.com>
2022-07-09 22:25:50 -0700

    commets

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from'./components/Greet'
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <h1 className="text-3xxl dark:bg-slate-800 bg-blue-200 font-bold underline">
         Hello world!
         this is a testasdasdagit 
       </h1>
+=======
+      <Greet/>
+>>>>>>> Stashed changes
     </div>
   );
 }
