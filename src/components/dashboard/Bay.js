920003f77fc12fe212bdb9f270d0ae893ed80dca
ZuciEC <zucidemartinez@gmail.com>
2022-07-09 23:13:19 -0700

    Bay css

---
+++ src/components/dashboard/Bay.js
@@ -0,0 +1,11 @@
+import './components/dashboard/Bay.css';
+
+function Bay() {
+    return(
+        <div className="Bay">
+            <h1> Zuci </h1>
+        </div>
+    );
+}
+
+export default Bay
