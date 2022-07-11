cbf7e6f0798492d6f2fd0b57e3579083452c4b17
Xeril-Lala <lopezlopezabel96@gmail.com>
2022-07-11 16:39:20 -0700

    navbar soft update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { slide as Menu } from 'react-burger-menu'
+import { elastic as Menu } from 'react-burger-menu'
 
 /*export const NavBar = () => {
     return <div className="bg-slate-300">panel goes here</div>
