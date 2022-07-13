017a96676212f83f663bdb4ccec5c492768c66d1
ZuciEC <zucidemartinez@gmail.com>
2022-07-12 19:34:59 -0700

    Update Bay.js

--- src/components/dashboard/Bay.js
+++ src/components/dashboard/Bay.js
@@ -1,99 +1,90 @@
-import './Bay.css';
-import React, {useState,useEffect} from "react";
-import { Fragment } from "react";
+import React, { Fragment,useState,useEffect } from "react";
+import {Doughnut} from 'react-chartjs-2';
 
+const Bay = ({bay}) => {
+    const { name,people,time,status, status:{icon}, chart:{percentage,current,goal} } = bay;
 
-const Bay = () => {
-    return(
+    const plugins = [{
+        beforeDraw: function(chart) {
+         var width = chart.width,
+             height = chart.height,
+             ctx = chart.ctx;
+             ctx.restore();
+             var fontSize = (height / 90).toFixed(2);
+             if(window.matchMedia('(prefers-color-scheme: dark)').matches)
+             ctx.fillStyle = '#FFFFFF';
+             ctx.font = fontSize + "em sans-serif";
+             ctx.textBaseline = "top";
+             if (chart.data.datasets[0]){
+                var text = `${percentage}`,
+                textX = Math.round((width - ctx.measureText(text).width) / 2),
+                textY = height / 2.4;
+                ctx.fillText(text, textX, textY);
+            }
+             ctx.save();
+        } 
+      }];
+
+      const options = {
+        responsive: true,
+        events: [],
+        cutout : '80%',
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+      };
+
+    //Loaded chart states
+    const [loadedChartData, setLoadedChartData] = useState({
+        datasets: [],
+    });
+
+    let statusIcon = status.id === 'LG' ? 'text-green-600'
+    : status.id === 'LG' ? 'text-green-600'
+    : status.id === 'UG' ? 'text-blue-600'
+    : status.id === 'WG' ? 'text-yellow-600'
+    : status.id === 'UE' ? 'text-black-600'
+    : status.id === 'text-black'
+
+    //Loaded chart setup
+    useEffect(() => {
+        setLoadedChartData({ 
+            labels: ["Meta completada"],
+            datasets : [
+                {
+                    label: "First",
+                    backgroundColor: ["#3B7FB6", "#e0e0e0"],
+                    data:[current,goal-current]
+                }
+            ]        
+        });
+    }, [current,goal]);
+
+    return ( 
         <Fragment>
-            <div className="content">
-                <div className="Bay">
-                    <h1 className="title"> Bahia 1 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i className="uil uil-arrow-circle-up"></i>
-                            <label className="time"> 10:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">3</label>
-                        </div>
+            <div className="card">
+                <h1 className="title">{name}</h1>
+                <div className="flex flex-row justify-between">
+                    <div className="ml-5 flex items-center ">
+                        <i className={`mr-1.5 uil uil-${icon} ${statusIcon}`}></i>
+                        <label className="text-lg"> {time} </label>
                     </div>
-                    <div className="chart"></div>
+                    <div className="mr-5 flex items-center">
+                        <i className="mr-1.5 uil uil-user-md"> </i>
+                        <label className="text-lg">{people}</label>
+                    </div>
                 </div>
-                <div className="Bay">
-                    <h1 className="title"> Bahia 2 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i className="uil uil-arrow-circle-down"> </i>
-                            <label className="time"> 10:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">3</label>
-                        </div>
-                    </div>
-                    <div className="chart"></div>
-                </div>
-                <div className="Bay">
-                    <h1 className="title"> Bahia 3 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i class="uil uil-clock"> </i>
-                            <label className="time"> 00:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">0</label>
-                        </div>
-                    </div>
-                    <div className="chart"></div>
-                </div>
-                <div className="Bay">
-                    <h1 className="title"> Bahia 4 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i className="uil uil-minus-circle"> </i>
-                            <label className="time"> 00:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">0</label>
-                        </div>
-                    </div>
-                    <div className="chart"></div>
-                </div>
-                <div className="Bay">
-                    <h1 className="title"> Bahia 5 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i className="uil uil-arrow-circle-down"> </i>
-                            <label className="time"> 07:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">5</label>
-                        </div>
-                    </div>
-                    <div className="chart"></div>
-                </div>
-                <div className="Bay">
-                    <h1 className="title"> Bahia 6 </h1>
-                    <div className="time-person">
-                        <div className="time-content">
-                            <i className="uil uil-arrow-circle-up"> </i>
-                            <label className="time">12:00 </label>
-                        </div>
-                        <div className="person-content">
-                            <i className="uil uil-user-md"> </i>
-                            <label className="person">2</label>
-                        </div>
-                    </div>
-                    <div className="chart"></div>
+                <div className="mx-auto w-9/12 2xl:w-44 p-[12px]">
+                <Doughnut data={loadedChartData} options={options} plugins={plugins}/>
+
                 </div>
             </div>
+            
         </Fragment>
-    );
-
+     );
 }
+ 
 export default Bay;
+
