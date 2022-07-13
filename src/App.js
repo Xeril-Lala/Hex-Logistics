97797f401db1d9a3bf27a3c9106311605e77deea
ZuciEC <zucidemartinez@gmail.com>
2022-07-12 19:33:57 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import Bay from './components/dashboard/Bay';
+import React from 'react';
+import Dashboard from './components/dashboard/Dashboard';
+import NavBar from "./components/Navbar";
+import "./Style.css";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Bay></Bay>
+    <div className="App bg-[#ebf1fd] dark:bg-zinc-900 dark:text-white ">
+      <NavBar/>
+
+      <Dashboard />
     </div>
   );
 }
