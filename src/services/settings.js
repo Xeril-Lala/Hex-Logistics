3488c224b2faa4aee84f7fb274623359aa651d2b
unknown <xlivorx@gmail.com>
2022-07-31 22:53:10 -0700

    Azure server

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://hexlogapi-001-site1.gtempurl.com';
+// export const API_URL = 'http://hexlogapi-001-site1.gtempurl.com';
 // export const API_URL = 'https://localhost:7043';
+export const API_URL = "https://hexapi.azurewebsites.net";
