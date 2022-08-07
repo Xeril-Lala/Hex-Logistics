7c5027c586c06ed131cea249377422a757b81734
unknown <xlivorx@gmail.com>
2022-08-07 13:58:19 -0700

    endpoints fixed

--- src/services/baysData.js
+++ src/services/baysData.js
@@ -6,25 +6,3 @@ export const getBaysData = async () => {
     .then((response)=>response.json())
     .catch((error)=> {console.log(error);})
 }
-
-export const getBay = async (id)=>{
-    var url = `${API_URL}/api/bays/${id}`;
-    return await fetch(url)
-    .then((response)=>response.json())
-    .catch((error)=>{console.log(error);} )
-}
-
-export const updateBay = async (id, updatedBay) =>{
-    var url = `${API_URL}/api/bays/${id}`;
-    return await fetch(url,{
-        method: 'PUT',
-        headers:{
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "guideNumber": parseInt(updatedBay.guideNumber),
-            "materialId": parseInt(updatedBay.materialId),
-            "personal": parseInt(updatedBay.personal),
-        })
-    })  
-}
