d8ccb33c42e5fa2a0426addf4a1494735a45c136
Xeril-Lala <lopezlopezabel96@gmail.com>
2022-08-08 16:11:35 -0700

    adding stuff to baysData.js

--- src/services/baysData.js
+++ src/services/baysData.js
@@ -6,3 +6,31 @@ export const getBaysData = async () => {
     .then((response)=>response.json())
     .catch((error)=> {console.log(error);})
 }
+
+export const postForm = async () => {
+    var url = `${API_URL}/api/bays/waiting/`;
+    return await fetch (url)
+    .then((response)=>response.json())
+    .catch((error)=> {console.log(error);})
+}
+
+export const postFormData = async ({postForm}) => {
+    var url = `${API_URL}/api/`;
+    return await fetch (url, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        // body: JSON.stringify({username,password})
+        body: JSON.stringify({
+            "materialType": String(postForm.materialType),
+            "password": String(password)
+        })
+    }).then(response=>{
+        if(!response.ok) throw new Error('Error in response')
+        return response.json()
+    }).then(response=>{
+        const  {token} = response
+        return token
+    })
+}
